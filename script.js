b2941d4a4196928cbd07d3fdff92a74cedb5c181
michrusinek <mich.rusinek@gmail.com>
2023-03-21 16:17:05 +0100

    Wrote a script that creates a 16x16 grid of green-border divs

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const size = 16;
 const container = document.querySelector('#container');
 container.setAttribute(
-    'style', `display: grid; grid-template-columns: repeat(${size}, 1fr [col-start]); grid-template-rows: repeat(${size}, 1fr [row-start])`
+    'style', `display: grid; 
+    grid-template-columns: repeat(${size}, 1fr [col-start]); 
+    grid-template-rows: repeat(${size}, 1fr [row-start])`
 )
+
+const div = document.createElement('div')
+
+for (let row = 1; row <= size; row++) {
+    for (let col = 1; col <= size; col++) {
+        const div = document.createElement('div')
+        div.classList.add('grid-box')
+        div.setAttribute(
+            'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1` 
+        )
+        container.appendChild(div) 
+    }
+}
+// col = 3
+// row = 1
+// div.classList.add('grid-box')
+// div.setAttribute(
+//     'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1` 
+// )
+// container.appendChild(div)
