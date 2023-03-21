46b6ce9b96196e851fddc5e517be9fea15304f7e
michrusinek <mich.rusinek@gmail.com>
2023-03-21 16:49:46 +0100

    Created a hover effect, that changes the color of divs to black

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-const size = 16;
-const container = document.querySelector('#container');
+
+const size = 32;
+const container = document.querySelector('#grid-container');
 container.setAttribute(
     'style', `display: grid; 
     grid-template-columns: repeat(${size}, 1fr [col-start]); 
     grid-template-rows: repeat(${size}, 1fr [row-start])`
-)
+);
 
-const div = document.createElement('div')
+const div = document.createElement('div');
 
 for (let row = 1; row <= size; row++) {
     for (let col = 1; col <= size; col++) {
         const div = document.createElement('div')
-        div.classList.add('grid-box')
         div.setAttribute(
-            'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1` 
-        )
-        container.appendChild(div) 
+            'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1`
+        );
+        div.setAttribute('class', 'grid-item');
+        container.appendChild(div);
     }
 }
-// col = 3
-// row = 1
-// div.classList.add('grid-box')
-// div.setAttribute(
-//     'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1` 
-// )
-// container.appendChild(div)
+
+const gridItems= document.querySelectorAll('.grid-item');
+gridItems.forEach((item) => {
+    item.addEventListener('mouseover', function() {
+        console.log("Hovered!")
+        item.classList.add('colored')
+    })
+})
