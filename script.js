bd1cf5a671ffb00fd954c2a168275323dce81b9d
michrusinek <mich.rusinek@gmail.com>
2023-03-22 13:58:41 +0100

    Added an input form to let the user change size of the sketch area

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
+let size = 16;
 
-const size = 32;
-const container = document.querySelector('#grid-container');
-container.setAttribute(
-    'style', `display: grid; 
-    grid-template-columns: repeat(${size}, 1fr [col-start]); 
-    grid-template-rows: repeat(${size}, 1fr [row-start])`
-);
+function createGrid(size) {
+    const container = document.querySelector('#grid-container');
+    container.setAttribute(
+        'style', `display: grid; 
+        grid-template-columns: repeat(${size}, 1fr [col-start]); 
+        grid-template-rows: repeat(${size}, 1fr [row-start])`
+    );
 
-const div = document.createElement('div');
-
-for (let row = 1; row <= size; row++) {
-    for (let col = 1; col <= size; col++) {
-        const div = document.createElement('div')
-        div.setAttribute(
-            'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1`
-        );
-        div.setAttribute('class', 'grid-item');
-        container.appendChild(div);
+    for (let row = 1; row <= size; row++) {
+        for (let col = 1; col <= size; col++) {
+            const div = document.createElement('div')
+            div.setAttribute(
+                'style', `grid-column: ${col} / span 1; grid-row: ${row} / span 1`
+            );
+            div.setAttribute('class', 'grid-item');
+            container.appendChild(div);
+        }
     }
+
+
 }
 
-const gridItems= document.querySelectorAll('.grid-item');
-gridItems.forEach((item) => {
-    item.addEventListener('mouseover', function() {
-        item.classList.add('colored')
+function addBlockColoring() {
+    const gridItems= document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) => {
+        item.addEventListener('mouseover', function() {
+            item.classList.add('colored');
+        })
     })
+}
+
+createGrid(size);
+addBlockColoring()
+
+const input = document.querySelector('input')
+
+input.addEventListener('input', function() {
+    size = parseInt(input.value);
+    createGrid(size);
+    addBlockColoring();
 })
